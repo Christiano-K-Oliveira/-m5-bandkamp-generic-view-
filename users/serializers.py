e53b879eeeb275889e6ec9952827863653929ac3
Christiano-K-Oliveira <kayky.20oliveira@gmail.com>
2023-04-26 15:41:09 -0300

    feat: Tarefa 1 - Projeto Legado

---
+++ users/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "password", "first_name", "last_name", "email", "is_superuser"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data: dict) -> User:
+        return User.objects.create_superuser(**validated_data)
+
+    def update(self, instance: User, validated_data: dict) -> User:
+        for key, value in validated_data.items():
+            if key == 'password':
+                instance.set_password(value)
+            else:
+                setattr(instance, key, value)
+
+        instance.save()
+
+        return instance
