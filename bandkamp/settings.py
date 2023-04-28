ff3c1c17c0641e3fff96c1babc663f09dd619148
Christiano-K-Oliveira <kayky.20oliveira@gmail.com>
2023-04-28 17:51:17 -0300

    fix: Tarefa 4

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -41,16 +41,6 @@ if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS +=[RENDER_EXTERNAL_HOSTNAME, "0.0.0.0"]
 
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-if DATABASE_URL:
-    db_from_env = dj_database_url.config(
-        default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-    DATABASES['default'].update(db_from_env)
-    DEBUG = False
-if not DEBUG:
-    # Tell Django to copy statics to the `staticfiles` directory
-    # in your application directory on Render.
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
@@ -123,6 +113,17 @@ DATABASES = {
     }
 }
 
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+if DATABASE_URL:
+    db_from_env = dj_database_url.config(
+        default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+    DATABASES['default'].update(db_from_env)
+    DEBUG = False
+if not DEBUG:
+    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
